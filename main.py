63907552fc38f91c4fcf1e7333f575c2ab804e90
Neel <npaul_be19@thapar.edu>
2022-02-06 23:08:06 +0530

    eye-ball and face detection code added

---
+++ main.py
@@ -0,0 +1,44 @@
+# import cv2
+# import numpy as np
+#
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#
+# img = cv2.imread('man4-side.jpg')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+# for (x, y, w, h) in faces:
+#     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+#
+# cv2.imshow('img',img)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+import cv2
+
+# Loading the cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# Defining a function that will do the detections
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    return frame
+
+# Doing some Face Recognition with the webcam
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+    cv2.imshow('Video', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
